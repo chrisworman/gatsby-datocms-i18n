889c46fa9197f664177ae0979ecbf6ccd7b4af47
Chris Worman <chris.worman@pelacase.com>
2020-04-03 14:19:45 -0700

    Removed commented out dead configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -91,8 +91,5 @@ module.exports = {
     //     includeCollections: ["shop"]
     //   }
     // }
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ]
 };
